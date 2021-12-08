f5dd45a1ddca4f2035b6e59b4b0b457cf521c00b
Raphael <rmc3408@protonmail.com>
2021-12-08 15:00:04 -0500

    video 12 done

---
+++ backend/keystone.ts
@@ -0,0 +1,28 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const sessionConfigured = {
+    maxAge: 60 * 60 * 24 * 100, //cookies will be avaliable for 100days
+    secret: process.env.COOKIE_SECRET
+}
+
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true
+        }
+    },
+    db: {
+        adapter: 'mongoose',
+        url: process.env.DATABASE_URL,
+        //add seed-data here
+    },
+    lists: createSchema({
+        //Schema items here
+    }),
+    ui: {
+        //add schema by user role
+        isAccessAllowed: () => true,
+    }
+});
