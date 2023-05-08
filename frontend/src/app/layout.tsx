e47219f5293b3641e361507661ff3130e12ea68b
Raphael <rmc3408@protonmail.com>
2023-05-08 10:54:47 -0400

    install styled-components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,15 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
+import StyledComponentsRegistry from '@src/lib/register'
+import './global.css'
 
 // Importing external fonts
 const inter = Inter({ subsets: ['latin'] })
 
 // Static metadata
 export const metadata: Metadata = {
-  title: 'Sick-fits Raphael Molinaro',
-  description: 'Next.js from wesBos course',
+  title: 'SickFits',
+  description: 'Next.js from WesBos Course and updated by Raphael Molinaro',
 }
 
 type rootProps = {
@@ -17,8 +18,10 @@ type rootProps = {
 
 export default function RootLayout({ children }: rootProps) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
-import styles from '@app/page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-      </div>
+    <main>
+      <h1>HOMEPAGE</h1>
+      <Image src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
     </main>
   )
 }
